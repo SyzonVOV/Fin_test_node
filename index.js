20f53a08dec15b1550f7c9c1100c4cdb8bde3ed2
SyzonVOV <you@example.com>
2020-10-28 12:37:02 +0200

    update PORT

--- index.js
+++ index.js
@@ -90,6 +90,6 @@ app.delete("/api/notes/:id", (request, response) => {
   response.json(note)
 }) */
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
