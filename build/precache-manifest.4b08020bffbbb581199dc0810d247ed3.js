61a07c74e9da60977543f989d74dfcbdfd9d6609
SyzonVOV <you@example.com>
2020-10-28 13:19:12 +0200

    added build

---
+++ build/precache-manifest.4b08020bffbbb581199dc0810d247ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa50965a9d923ccb799bdbb64f7c0d41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d5dc60b7f8add06a77",
+    "url": "/static/css/main.603adb8d.chunk.css"
+  },
+  {
+    "revision": "76596f5380ee97ca362b",
+    "url": "/static/js/2.6609d15c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6609d15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d5dc60b7f8add06a77",
+    "url": "/static/js/main.96283223.chunk.js"
+  },
+  {
+    "revision": "8dc0203a9e40d0f2c0ac",
+    "url": "/static/js/runtime-main.5a039d6f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 
 let notes = [
