d934e80c18b2015da3664d77e71a7a45e2301e09
SyzonVOV <you@example.com>
2020-11-06 16:30:38 +0200

    upto 3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const Note = require('./models/note')
 
 app.use(cors());
 app.use(express.static('build'));
@@ -39,18 +41,16 @@ const generateId = () => {
 }
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes)
+  });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
 
 app.post("/api/notes", (request, response) => {
   const body = request.body
@@ -61,16 +61,16 @@ app.post("/api/notes", (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
     id: generateId(),
-  }
+  });
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -89,8 +89,9 @@ app.delete("/api/notes/:id", (request, response) => {
   })
   console.log(note)
   response.json(note)
-}) */
+}) 
+*/
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
